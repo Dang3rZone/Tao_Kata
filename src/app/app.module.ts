fc9067fd242e534eb2471e6e9f4e4c2339c8df18
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-01 16:52:44 +0200

    removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { CharactersComponent } from './components/characters/characters.component';
-import { EpisodesComponent } from './components/episodes/episodes.component';
+
 import { CharacterViewComponent } from './components/character-view/character-view.component';
 import { FormsModule } from '@angular/forms';
 
@@ -18,16 +18,10 @@ import { FormsModule } from '@angular/forms';
     FooterComponent,
     HeroComponent,
     CharactersComponent,
-    EpisodesComponent,
-    CharacterViewComponent
+    CharacterViewComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,19 +4,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  search: String
-  constructor(
-    private router: Router
-  ) { }
+  search: String;
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   searchValue() {
     this.router.navigate(['search', this.search]);
   }
-
 }
